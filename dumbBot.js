7e826921befdf5e16825b36e70935a253c40d4f3
Evgeniy Bondarenko <evgeniy.bondarenko@pay-me.ru>
2017-04-26 10:25:20 +0300

    small fixes

--- dumbBot.js
+++ dumbBot.js
@@ -11,42 +11,26 @@ var QuestBot = function (bot, config) {
   bot.on('webhook_error', (error) => {
     console.log("Webhook error: " + error.code);  // => 'EPARSE'
   });
-
-// Matches "/echo [whatever]"
-  /*bot.onText(/\/echo (.+)/, function (msg, match) {
-   // 'msg' is the received Message from Telegram
-   // 'match' is the result of executing the regexp above on the text content
-   // of the message
-
-   var chatId = msg.from.id;
-   var resp = match[1]; // the captured "whatever"
-
-   console.log(msg);
-   // send back the matched "whatever" to the chat
-   bot.sendMessage(chatId, resp);
-   });
-   */
   bot.on('message', function (msg) {
     console.log(msg);
     var chatId = msg.chat.id;
-    childProcess.exec('cd neuro && python sample.py --prime "' + msg.text + '" -n 50', null, (error, stdout, stderr)=> {
+    let text = " ";
+    if (msg.text != undefined) {
+      text = msg.text.replace(/['"]+/g, '');
+      text = text.replace(/\n+/g, ' ');
+    }
+    bot.sendChatAction(chatId, 'typing');
+    childProcess.exec('cd neuro && python sample.py --prime "' + text + '" -n 50', null, (error, stdout, stderr)=> {
       if (stdout.toString().length > 0) {
         let data = stdout.trim();
-        let pos1 = data.lastIndexOf("\n");
-        console.log("!" + pos1 + "!");
-        data = data.substr(pos1);
+        data = data.substr(data.lastIndexOf("\n"));
+        let minLen = [data.indexOf('.', 20), data.indexOf('?', 20), data.indexOf('!', 20)];
+        minLen = minLen.filter((val)=> {
+          return val !== -1;
+        });
         let finish = data.length;
-        let pos = data.indexOf('.', 20);
-        if (pos !== -1 && pos < finish) {
-          finish = pos + 1;
-        }
-        pos = data.indexOf('?', 20);
-        if (pos !== -1 && pos < finish) {
-          finish = pos + 1;
-        }
-        pos = data.indexOf('!', 20);
-        if (pos !== -1 && pos < finish) {
-          finish = pos + 1;
+        if (minLen.length !== 0) {
+          finish = Math.min.apply(null, minLen) + 1;
         }
         data = data.substr(data, finish);
         bot.sendMessage(chatId, data);
